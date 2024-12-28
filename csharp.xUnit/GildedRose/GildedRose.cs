4ee784820d0c17146c5532070cfb5f1d97f8faae
Maxence <Maxence.MARTIN@consulting-for.edenred.com>
2024-12-28 11:51:09 +0100

    Extract UpdateQuality in Item classe

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -8,65 +8,7 @@ public class GildedRose(IList<Item> Items)
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            var currentItem = Items[i];
-            
-            if (currentItem.NameIsNot("Aged Brie") && currentItem.NameIsNot("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                if (currentItem.Quality > 0 && currentItem.NameIsNot("Sulfuras, Hand of Ragnaros"))
-                {
-                    currentItem.Quality -= 1;
-                }
-            }
-            else
-            {
-                if (currentItem.Quality < 50)
-                {
-                    currentItem.Quality += 1;
-
-                    if (currentItem.NameIs("Backstage passes to a TAFKAL80ETC concert"))
-                    {
-                        if (currentItem.SellIn < 11 && currentItem.Quality < 50)
-                        {
-                            currentItem.Quality += 1;
-                        }
-
-                        if (currentItem.SellIn < 6 && currentItem.Quality < 50)
-                        {
-                            currentItem.Quality += 1;
-                        }
-                    }
-                }
-            }
-
-            if (currentItem.NameIsNot("Sulfuras, Hand of Ragnaros"))
-            {
-                currentItem.SellIn -= 1;
-            }
-
-            if (currentItem.SellIn < 0)
-            {
-                if (currentItem.NameIsNot("Aged Brie"))
-                {
-                    if (currentItem.NameIsNot("Backstage passes to a TAFKAL80ETC concert"))
-                    {
-                        if (currentItem.Quality > 0 && currentItem.NameIsNot("Sulfuras, Hand of Ragnaros"))
-                        {
-                            currentItem.Quality -= 1;
-                        }
-                    }
-                    else
-                    {
-                        currentItem.Quality -= currentItem.Quality;
-                    }
-                }
-                else
-                {
-                    if (currentItem.Quality < 50)
-                    {
-                        currentItem.Quality += 1;
-                    }
-                }
-            }
+            Items[i].UpdateQuality();
         }
     }
 }
--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -5,4 +5,90 @@ public class Item
     public string Name { get; set; }
     public int SellIn { get; set; }
     public int Quality { get; set; }
+
+    public void UpdateQuality()
+    {
+        if (NameIsNot("Aged Brie") && NameIsNot("Backstage passes to a TAFKAL80ETC concert"))
+        {
+            if (Quality > 0 && NameIsNot("Sulfuras, Hand of Ragnaros"))
+            {
+                DecreaseQuality();
+            }
+        }
+        else
+        {
+            if (Quality < 50)
+            {
+                IncreaseQuality();
+
+                if (NameIs("Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if (SellIn < 11 && Quality < 50)
+                    {
+                        IncreaseQuality();
+                    }
+
+                    if (SellIn < 6 && Quality < 50)
+                    {
+                        IncreaseQuality();
+                    }
+                }
+            }
+        }
+
+        if (NameIsNot("Sulfuras, Hand of Ragnaros"))
+        {
+            SellIn -= 1;
+        }
+
+        if (SellIn < 0)
+        {
+            if (NameIsNot("Aged Brie"))
+            {
+                if (NameIsNot("Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if (Quality > 0 && NameIsNot("Sulfuras, Hand of Ragnaros"))
+                    {
+                        DecreaseQuality();
+                    }
+                }
+                else
+                {
+                    DecreaseQualityByCurrentQuality();
+                }
+            }
+            else
+            {
+                if (Quality < 50)
+                {
+                    IncreaseQuality();
+                }
+            }
+        }
+    }
+
+    private bool NameIs(string name)
+    {
+        return Name == name;
+    }
+
+    private bool NameIsNot(string name)
+    {
+        return !NameIs(name);
+    }
+
+    private void IncreaseQuality()
+    {
+        Quality += 1;
+    }
+
+    private void DecreaseQuality()
+    {
+        Quality -= 1;
+    }
+
+    private void DecreaseQualityByCurrentQuality()
+    {
+        Quality -= Quality;
+    }
 }
